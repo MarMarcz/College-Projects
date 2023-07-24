60dedfee05755d6c78f240518d45ba434d2a27b2
MarMarcz <91740757+MarMarcz@users.noreply.github.com>
2023-07-24 13:36:55 +0200

    Delete projectjaz directory

--- projectjaz/src/main/java/com/example/projectjaz/exception/AuthorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectjaz.exception;
-
-public class AuthorNotFoundException extends RuntimeException {
-    public AuthorNotFoundException(Long id) {
-        super("Author id : " + id + " does not exist");
-    }
-}
--- projectjaz/src/main/java/com/example/projectjaz/repository/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.projectjaz.repository;
-
-import com.example.projectjaz.entity.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-
-}
